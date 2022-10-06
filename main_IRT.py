b9b1dcddd53ca29e10327c2f545e6d1c9198d73a
delldu <18588220928@163.com>
2022-10-06 18:26:54 +0800

    Study

--- main_IRT.py
+++ main_IRT.py
@@ -8,19 +8,19 @@ import torch
 import torch.nn as nn
 import numpy as np
 from glob import glob
-import scipy.misc as sic
+# import scipy.misc as sic
 import subprocess
 import models.network as net
 import argparse
 import random
 from vgg import VGG19
+import imageio
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--model", default='Test', type=str, help="Name of model")
-parser.add_argument("--save_freq", default=5, type=int, help="save frequency of epochs")
+parser.add_argument("--save_freq", default=1, type=int, help="save frequency of epochs")
 parser.add_argument("--use_gpu", default=1, type=int, help="use gpu or not")
 parser.add_argument("--with_IRT", default=0, type=int, help="use IRT or not")
-parser.add_argument("--IRT_initialization", default=0, type=int, help="use initialization for IRT or not")
 parser.add_argument("--max_epoch", default=25, type=int, help="The max number of epochs for training")
 parser.add_argument("--input", default='./demo/colorization/goat_input', type=str, help="dir of input video")
 parser.add_argument("--processed", default='./demo/colorization/goat_processed', type=str, help="dir of processed video")
@@ -41,7 +41,7 @@ processed_folder = ARGS.processed
 with_IRT = ARGS.with_IRT
 maxepoch = ARGS.max_epoch + 1
 model=  ARGS.model
-task = "/{}_IRT{}_initial{}".format(model, with_IRT, ARGS.IRT_initialization) #Colorization, HDR, StyleTransfer, Dehazing
+task = "/{}_IRT{}".format(model, with_IRT) #Colorization, HDR, StyleTransfer, Dehazing
 
 # set gpu
 if ARGS.use_gpu:
@@ -92,14 +92,18 @@ input_folders = [input_folder]
 processed_folders = [processed_folder]
 
 def prepare_paired_input(task, id, input_names, processed_names, is_train=0):
-    net_in = np.float32(scipy.misc.imread(input_names[id]))/255.0
+    # net_in = np.float32(scipy.misc.imread(input_names[id]))/255.0
+    net_in = np.float32(imageio.imread(input_names[id]))/255.0
+
     if len(net_in.shape) == 2:
         net_in = np.tile(net_in[:,:,np.newaxis], [1,1,3])
-    net_gt = np.float32(scipy.misc.imread(processed_names[id]))/255.0
+    # net_gt = np.float32(scipy.misc.imread(processed_names[id]))/255.0
+    net_gt = np.float32(imageio.imread(processed_names[id]))/255.0
+
     org_h,org_w = net_in.shape[:2]   
     h = org_h // 32 * 32
     w = org_w // 32 * 32
-    print(net_in.shape, net_gt.shape)
+    # print(net_in.shape, net_gt.shape)
     return net_in[np.newaxis, :h, :w, :], net_gt[np.newaxis, :h, :w, :]
 
 # some functions 
@@ -111,7 +115,7 @@ def initialize_weights(model):
             if module.bias is not None:
                 module.bias.data.zero_()
         elif isinstance(module, nn.BatchNorm2d):
-            module.weight.data.fill_(1)
+            module.weight.data.fill_(1.0)
             module.bias.data.zero_()
 
 def normalize_batch(batch):
@@ -130,7 +134,7 @@ for folder_idx, input_folder in enumerate(input_folders):
         output_folder = "./result/{}".format(task + '/' + input_folder.split("/")[-2] + '/' + input_folder.split("/")[-1]) 
     else:
         output_folder = ARGS.output + "/" + task + '/' + input_folder.split("/")[-1] 
-    print(output_folder, input_folders[folder_idx], processed_folders[folder_idx] )
+    # print(output_folder, input_folders[folder_idx], processed_folders[folder_idx] )
     
     num_of_sample = min(len(input_names), len(processed_names))
     data_in_memory = [None] * num_of_sample                                                 #Speedup
@@ -155,26 +159,19 @@ for folder_idx, input_folder in enumerate(input_folders):
         if not os.path.isdir("{}/training".format(output_folder)):
             os.makedirs("{}/training".format(output_folder))
 
-        print(len(input_names), len(processed_names))
+        # print(len(input_names), len(processed_names))
         for id in range(num_of_sample): 
             if with_IRT:      
-                if epoch < 6 and ARGS.IRT_initialization:
-                    net_in,net_gt = data_in_memory[0]      #Option: 
-                    prediction = net(net_in)
-                    
-                    crt_loss = loss_L1(prediction[:,:3,:,:], net_gt) + 0.9*loss_L1(prediction[:,3:,:,:], net_gt)
-
-                else:
-                    net_in,net_gt = data_in_memory[id]
-                    prediction = net(net_in)
-                    
-                    prediction_main = prediction[:,:3,:,:]
-                    prediction_minor = prediction[:,3:,:,:]
-                    diff_map_main,_ = torch.max(torch.abs(prediction_main - net_gt) / (net_in+1e-1), dim=1, keepdim=True)
-                    diff_map_minor,_ = torch.max(torch.abs(prediction_minor - net_gt) / (net_in+1e-1), dim=1, keepdim=True)
-                    confidence_map = torch.lt(diff_map_main, diff_map_minor).repeat(1,3,1,1).float()
-                    crt_loss = loss_L1(prediction_main*confidence_map, net_gt*confidence_map) \
-                            + loss_L1(prediction_minor*(1-confidence_map), net_gt*(1-confidence_map))
+                net_in,net_gt = data_in_memory[id]
+                prediction = net(net_in)
+                
+                prediction_main = prediction[:,:3,:,:]
+                prediction_minor = prediction[:,3:,:,:]
+                diff_map_main,_ = torch.max(torch.abs(prediction_main - net_gt) / (net_in+1e-1), dim=1, keepdim=True)
+                diff_map_minor,_ = torch.max(torch.abs(prediction_minor - net_gt) / (net_in+1e-1), dim=1, keepdim=True)
+                confidence_map = torch.lt(diff_map_main, diff_map_minor).repeat(1,3,1,1).float()
+                crt_loss = loss_L1(prediction_main*confidence_map, net_gt*confidence_map) \
+                        + loss_L1(prediction_minor*(1-confidence_map), net_gt*(1-confidence_map))
             else:
                 net_in,net_gt = data_in_memory[id] 
                 prediction = net(net_in)
@@ -194,7 +191,7 @@ for folder_idx, input_folder in enumerate(input_folders):
                 prediction = prediction.detach().permute(0,2,3,1).cpu().numpy()
                 if with_IRT:
                     prediction = prediction[...,:3]
-                sic.imsave("{}/training/step{:06d}_{:06d}.jpg".format(output_folder, step, id), 
+                imageio.imsave("{}/training/step{:06d}_{:06d}.jpg".format(output_folder, step, id), 
                            np.uint8(np.concatenate([net_in[0], prediction[0], net_gt[0]], axis=1).clip(0,1) * 255.0))      
 
         # # -----------save intermidiate results-------------
@@ -202,7 +199,7 @@ for folder_idx, input_folder in enumerate(input_folders):
             for id in range(num_of_sample):
                 st=time.time()
                 net_in,net_gt = data_in_memory[id]
-                print("Test: {}-{} \r".format(id, num_of_sample))
+                # print("Test: {}-{} \r".format(id, num_of_sample))
 
                 with torch.no_grad():
                     prediction = net(net_in)                
@@ -217,15 +214,15 @@ for folder_idx, input_folder in enumerate(input_folders):
                     diff_map_minor = np.amax(np.absolute(prediction_minor - net_gt) / (net_in+1e-1), axis=3, keepdims=True)                    
                     confidence_map = np.tile(np.less(diff_map_main, diff_map_minor), (1,1,1,3)).astype('float32')
 
-                    sic.imsave("{}/{:04d}/predictions_{:05d}.jpg".format(output_folder, epoch, id),
+                    imageio.imsave("{}/{:04d}/predictions_{:05d}.jpg".format(output_folder, epoch, id),
                         np.uint8(np.concatenate([net_in[0,:,:,:3],prediction_main[0], prediction_minor[0],net_gt[0], confidence_map[0]], axis=1).clip(0,1) * 255.0))
-                    sic.imsave("{}/{:04d}/out_main_{:05d}.jpg".format(output_folder, epoch, id),np.uint8(prediction_main[0].clip(0,1) * 255.0))               
-                    sic.imsave("{}/{:04d}/out_minor_{:05d}.jpg".format(output_folder, epoch, id),np.uint8(prediction_minor[0].clip(0,1) * 255.0))   
+                    imageio.imsave("{}/{:04d}/out_main_{:05d}.jpg".format(output_folder, epoch, id),np.uint8(prediction_main[0].clip(0,1) * 255.0))               
+                    imageio.imsave("{}/{:04d}/out_minor_{:05d}.jpg".format(output_folder, epoch, id),np.uint8(prediction_minor[0].clip(0,1) * 255.0))   
 
                 else:
 
-                    sic.imsave("{}/{:04d}/predictions_{:05d}.jpg".format(output_folder, epoch, id), 
+                    imageio.imsave("{}/{:04d}/predictions_{:05d}.jpg".format(output_folder, epoch, id), 
                         np.uint8(np.concatenate([net_in[0,:,:,:3], prediction[0], net_gt[0]],axis=1).clip(0,1) * 255.0))
-                    sic.imsave("{}/{:04d}/out_main_{:05d}.jpg".format(output_folder, epoch, id), 
+                    imageio.imsave("{}/{:04d}/out_main_{:05d}.jpg".format(output_folder, epoch, id), 
                         np.uint8(prediction[0].clip(0,1) * 255.0))
 
--- test.sh
+++ test.sh
@@ -1,4 +1,34 @@
-python main_IRT.py --max_epoch 50 --input demo/colorization/goat_input --processed demo/colorization/goat_processed --model colorization --with_IRT 1 --IRT_initialization 1 --output ./result/colorization
-python main_IRT.py --max_epoch 25 --input demo/Enhancement/bike-packing-input --processed demo/Enhancement/bike-packing-processed --model enhancement --output ./result/enhancement
-python main_IRT.py --max_epoch 25 --input demo/Dehazing/Haze2_input --processed demo/Dehazing/Haze2_processed/ --model dehazing --output ./result/dehazing
-python main_IRT.py --max_epoch 25 --input demo/spatial_white_balance/Bedroom_input --processed demo/spatial_white_balance/Bedroom_processed --model spatial_white_balance --output ./result/spatial_white_balance --with_IRT 1 --IRT_initialization 1
+MAX_EPOCH=5
+WITH_IRT=0
+
+time python main_IRT.py \
+    --max_epoch ${MAX_EPOCH} \
+    --input demo/colorization/goat_input \
+    --processed demo/colorization/goat_processed \
+    --model colorization \
+    --with_IRT ${WITH_IRT} \
+    --output ./result/colorization
+    
+# time python main_IRT.py \
+#     --max_epoch ${MAX_EPOCH} \
+#     --input demo/Enhancement/bike-packing-input \
+#     --processed demo/Enhancement/bike-packing-processed \
+#     --model enhancement \
+#     --with_IRT ${WITH_IRT} \
+#     --output ./result/enhancement
+
+# time python main_IRT.py \
+#     --max_epoch ${MAX_EPOCH} \
+#     --input demo/Dehazing/Haze2_input \
+#     --processed demo/Dehazing/Haze2_processed/ \
+#     --model dehazing \
+#     --with_IRT ${WITH_IRT} \
+#     --output ./result/dehazing
+
+# time python main_IRT.py \
+#     --max_epoch ${MAX_EPOCH} \
+#     --input demo/spatial_white_balance/Bedroom_input \
+#     --processed demo/spatial_white_balance/Bedroom_processed \
+#     --model spatial_white_balance \
+#     --with_IRT ${WITH_IRT} \
+#     --output ./result/spatial_white_balance
